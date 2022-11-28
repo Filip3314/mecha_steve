a14fc8cb022b09d4b2edced25140ea77824495fe
Filip Musial <fmusial@outlook.com>
2022-11-27 19:16:02 -0500

    Restructured tests dir

--- tests/music/test_utils.py
+++ tests/music/test_utils.py
@@ -2,11 +2,11 @@ import mecha_steve.music.utils as utils
 import pytest
     
 @pytest.mark.asyncio
-async def test_find_audio_online_empty_arg():
+async def test_empty_arg():
     assert await utils.find_audio_online("") == None
 
 @pytest.mark.asyncio
-async def test_find_audio_online_valid_youtube_url():
+async def test_youtube_url():
     assert "Never Gonna Give You Up" in (await utils.find_audio_online("https://www.youtube.com/watch?v=dQw4w9WgXcQ")).title
     assert "Never Gonna Give You Up" in (await utils.find_audio_online("www.youtube.com/watch?v=dQw4w9WgXcQ")).title
     assert "sniffer.avi" == (await utils.find_audio_online("https://www.youtube.com/watch?v=SCZI7fZJlgE")).title
@@ -16,14 +16,18 @@ async def test_find_audio_online_valid_youtube_url():
 
 
 @pytest.mark.asyncio
-async def test_find_audio_online_search():
+async def test_search():
     assert "Never Gonna Give You Up" in (await utils.find_audio_online("youtube never gonna give you up")).title
     assert "sniffer.avi" == (await utils.find_audio_online("sniffer.avi")).title
 
 @pytest.mark.asyncio
-async def test_find_audio_online_invalid_url():
-    return
+async def test_connetion_error():
+    assert "https://sadfjdslkj.com" == (await utils.find_audio_online("sniffer.avi")).title
+
+@pytest.mark.asyncio
+async def test_404():
+    assert await utils.find_audio_online("youtube.com/bruh")
     
 @pytest.mark.asyncio
-async def test_find_audio_online_spotify_url():
+async def test_spotify_url():
    assert "Gas Gas Gas" in (await utils.find_audio_online("https://open.spotify.com/album/0uhTliVFDT7CCzitqtW4KA")).title
